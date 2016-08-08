6ccf06581c981e1e1f025e9f617f0348739f70f2
spennihana <spnrpa@gmail.com>
2016-08-08 14:33:50 -0700

    push source code and Library

---
+++ CustomCursor/Cursor101/Assets/MyCursor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCursor : MonoBehaviour {
+    private MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start () {
+        meshRenderer = this.gameObject.GetComponent<MeshRenderer>();
+        meshRenderer.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // show the cursor where the user's gaze is
+        this.gameObject.transform.position = Camera.main.transform.position;
+	}
+}
